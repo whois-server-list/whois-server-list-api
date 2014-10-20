97b58b4b1f1864360e0b10517f72f80e9b129c9e
Markus Malkusch <markus@malkusch.de>
2014-10-20 22:48:08 +0200

    run tests

--- src/test/java/de/malkusch/whoisServerList/api/v1/DomainListFactoryTest.java
+++ src/test/java/de/malkusch/whoisServerList/api/v1/DomainListFactoryTest.java
@@ -7,12 +7,10 @@ import java.net.URL;
 
 import javax.xml.bind.JAXBException;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import de.malkusch.whoisServerList.api.v1.model.DomainList;
 
-@Ignore
 public class DomainListFactoryTest {
 
     @Test
