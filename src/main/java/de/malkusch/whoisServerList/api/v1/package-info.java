974d4ec2adba28daad6b0c4161957a27520b553a
Markus Malkusch <markus@malkusch.de>
2014-10-28 20:18:27 +0100

    package-info

---
+++ src/main/java/de/malkusch/whoisServerList/api/v1/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Whois server API.
+ *
+ * @author markus@malkusch.de
+ * @see <a href="bitcoin:1335STSwu9hST4vcMRppEPgENMHD2r1REK">Donations</a>
+ */
+package de.malkusch.whoisServerList.api.v1;
